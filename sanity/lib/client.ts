60a69609ac51e38dfd65b819f0e5a7a32d43340c
kafuideyinterviews <kafuideyinterviews3@gmail.com>
2026-05-09 09:33:37 +0000

    Add files via upload

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -2,7 +2,7 @@ import 'server-only'
 import { draftMode } from 'next/headers'
 import { createClient } from 'next-sanity'
 
-export const projectId  = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+export const projectId  = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'notset'
 export const dataset    = process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production'
 export const apiVersion = '2024-01-01'
 
